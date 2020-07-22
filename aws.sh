9494a0aa294f4ca88e1be0a1c524261a4d891bb8
Siddhant <ssiddhant3030@gmail.com>
2020-07-22 14:13:12 +0530

    commands added

--- aws.sh
+++ aws.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # install httpd (Linux 2 version)
+
+# aws steps
 yum update -y
 yum install -y httpd.x86_64
 systemctl start httpd.service
 systemctl enable httpd.service
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
