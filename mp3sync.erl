3f2b24c527ecc9ba599ec4c1d4503734d2baa111
Siddhant <ssiddhant3030@gmail.com>
2020-07-26 01:39:20 +0530

    synchronization frame in MPEG Data

---
+++ mp3sync.erl
@@ -0,0 +1,19 @@
+-module(mp3sync).
+-export([find_sync/1]).
+
+
+
+find_sync(Bin, N) ->
+case is_header(N, Bin) of
+{ok, Len1, _} ->
+case is_header(N + Len1, Bin) of
+{ok, Len2, _} ->
+case is_header(N + Len1 + Len2, Bin) of
+{ok, _, _} -> {ok, N};
+error ->
+find_sync(Bin, N+1)
+end; error ->
+ end.
+end; error ->
+        find_sync(Bin, N+1)
+find_sync(Bin, N+1)
