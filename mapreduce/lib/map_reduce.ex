0192448b704987b15f5aa7538bc98022619cf1f1
Siddhant <ssiddhant3030@gmail.com>
2020-08-06 07:19:22 +0530

    map reduce in elixir

---
+++ mapreduce/lib/map_reduce.ex
@@ -0,0 +1,18 @@
+defmodule MapReduce do
+  @moduledoc """
+  Documentation for `MapReduce`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MapReduce.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
